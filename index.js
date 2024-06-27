f9393c1f1d7cb7d3722ba4a82e578d6d3e840152
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-06-27 12:01:09 +0300

    nesting and calling functions feels good

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+let mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+function wrapAdjective(wrap = "*") {
+  const inner = function (me = "special") {
+    return `You are ${wrap}${me}${wrap}!`;
+  };
+  return inner;
+}
+let mess = wrapAdjective("%")("a dedicated programmer");
+console.log(mess);
